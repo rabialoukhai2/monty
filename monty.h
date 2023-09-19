b8b06e990829e9663630ec2536f88e0709ac7b96
rabialoukhai2 <134720091+rabialoukhai2@users.noreply.github.com>
2023-09-19 14:26:06 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -16,8 +16,8 @@
 typedef struct stack_s
 {
 	int n;
-	struct stack_s * prev;
-	struct stack_s * next;
+	struct stack_s *prev;
+	struct stack_s *next;
 }
 
 stack_t;
@@ -33,7 +33,7 @@ stack_t;
 typedef struct instruction_s
 {
 	char *opcode;
-	void(*f)(stack_t **stack, unsigned int line_number);
+	void (*f)(stack_t **stack, unsigned int line_number);
 }
 
 instruction_t;
