d915c5f111283b6875153d46f79f54e0f05216bd
rabialoukhai2 <rloukhi@gmail.com>
2023-09-19 04:29:52 -0700

    commit task

---
+++ pall.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/* Function to print all values on the stack */
+void pall(stack_t **stack, unsigned int line_number)
+{
+stack_t *current = *stack;
+/* Check if the stack is empty */
+if (current == NULL)
+{
+return;  // Nothing to print
+}
+
+/* Traverse the stack and print all values */
+while (current != NULL) 
+{
+printf("%d\n", current->n);
+current = current->next;
+}
+}
