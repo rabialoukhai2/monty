36689e6803fb68b9f8ca7a5fc9e7ccdca9a5a7c3
rabialoukhai2 <rloukhi@gmail.com>
2023-09-19 06:15:22 -0700

    commit task

---
+++ pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ *pint - Prints the value at the top of the stack.
+ *@stack: A pointer to the stack.
+ *@line_number: The line number in the Monty bytecode file.
+ *
+ *Return: Nothing.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
