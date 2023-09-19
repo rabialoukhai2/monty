6efa9d8653eaaf4b9ada18c3b60b765eabb8f4ce
rabialoukhai2 <rloukhi@gmail.com>
2023-09-19 06:30:12 -0700

    commit task

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ *nop - Doesn't do anything.
+ *@stack: A pointer to the stack.
+ *@line_number: The line number in the Monty bytecode file.
+ *
+ *Return: Nothing.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
