0a4a826570dc5c3cc14420196a85547f513c8534
rabialoukhai2 <134720091+rabialoukhai2@users.noreply.github.com>
2023-09-19 14:11:32 +0100

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,20 +1,22 @@
 #include "monty.h"
 
 /**
- * pall - Prints all values on the stack.
- * @stack: A pointer to the stack.
- * @line_number: The line number in the Monty bytecode file.
+ *pall - Prints all values on the stack.
+ *@stack: A pointer to the stack.
+ *@line_number: The line number in the Monty bytecode file.
  *
- * Return: Nothing.
+ *Return: Nothing.
  */
-void pall(stack_t ** stack, unsigned int line_number) {
-  stack_t * current = * stack;
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
 
-  if (current == NULL)
-    return;
+	if (current == NULL)
+		return;
 
-  while (current != NULL) {
-    printf("%d\n", current -> n);
-    current = current -> next;
-  }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
