fa73fa288099bab39eb13c440e39f31e25b3cadf
rabialoukhai2 <rloukhi@gmail.com>
2023-09-19 06:22:44 -0700

    commit task

--- monty.h
+++ monty.h
@@ -43,6 +43,6 @@ void push(stack_t **stack, unsigned int line_number, int value);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
-
+void swap(stack_t **stack, unsigned int line_number);
 
 #endif /*MONTY_H */
