54d7cd63d29118e2760825aa44a82258bba77889
rabialoukhai2 <rloukhi@gmail.com>
2023-09-20 03:04:40 -0700

    commit task

--- monty.h
+++ monty.h
@@ -1,7 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include <ctype.h>
 
 /**
@@ -39,6 +43,8 @@ typedef struct instruction_s
 instruction_t;
 
 /*Function prototypes for opcode handlers */
+
+int main(int argc, char *argv[]);
 void push(stack_t **stack, unsigned int line_number, int value);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
