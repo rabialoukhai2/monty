80958202631c5ecf06ccf7978e6748ca42d1d7f5
rabialoukhai2 <rloukhi@gmail.com>
2023-09-20 03:44:21 -0700

    commit task

--- push.c
+++ push.c
@@ -2,34 +2,23 @@
 
 /**
  *push - Pushes an element onto the stack.
- *@stack: A pointer to the stack.
- *@line_number: The line number in the Monty bytecode file.
- *@value: The integer value to push onto the stack.
- *
- *Return: Nothing.
+ *@stack: Pointer to the stack.
+ *@value: The value to push onto the stack.
  */
-void push(stack_t **stack, unsigned int line_number, int value)
+void push(stack_t **stack, int value)
 {
-	stack_t *new_node;
-
-	if (!isdigit(value) && (value[0] != '-' && !isdigit(value[1])))
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+	stack_t *new_node = malloc(sizeof(stack_t));
+	if (!new_node)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	new_node->n = atoi(value);
+	new_node->n = value;
 	new_node->next = *stack;
 	new_node->prev = NULL;
 
-	if (*stack != NULL)
+	if (*stack)
 		(*stack)->prev = new_node;
 
 	*stack = new_node;
