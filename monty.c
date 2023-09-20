67f7204db7530ed99eb365cd7af21cb1e19be87d
rabialoukhai2 <rloukhi@gmail.com>
2023-09-20 03:37:59 -0700

    commit task

--- monty.c
+++ monty.c
@@ -1,41 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "monty.h"
 
 /**
- *main - Monty bytecode interpreter
+ *error_usage - Prints the usage message and exits.
+ */
+void error_usage(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
+}
+
+/**
+ *file_error - Prints a file error message and exits.
+ *@argv: The name of the file causing the error.
+ */
+void file_error(char *argv)
+{
+	fprintf(stderr, "Error: Can't open file %s\n", argv);
+	exit(EXIT_FAILURE);
+}
+
+int status = 0;
+
+/**
+ *main - Entry point of the Monty interpreter.
  *@argc: The number of command-line arguments.
  *@argv: An array of command-line argument strings.
  *
- *Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
+ *Return: 0 on success, EXIT_FAILURE on failure.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	FILE *file;
-	char *line = NULL;
-	size_t len = 0;
-	unsigned int line_number = 0;
+	FILE * file;
+	size_t buf_len = 0;
+	char *buffer = NULL;
+	char *str = NULL;
 	stack_t *stack = NULL;
+	unsigned int line_cnt = 1;
+
+	global.data_struct = 1;
 
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+		error_usage();
 
 	file = fopen(argv[1], "r");
-	if (file == NULL)
+
+	if (!file)
+		file_error(argv[1]);
+
+	while (getline(&buffer, &buf_len, file) != -1)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
+		if (status)
+			break;
+
+		if (*buffer == '\n')
+		{
+			line_cnt++;
+			continue;
+		}
+
+		str = strtok(buffer, " \t\n");
+
+		if (!str || *str == '#')
+		{
+			line_cnt++;
+			continue;
+		}
+
+		global.argument = strtok(NULL, " \t\n");
+		opcode(&stack, str, line_cnt);
+		line_cnt++;
 	}
 
-	while (getline(&line, &len, file) != -1)
-	{
-		line_number++;
-		process_line(line, &stack, line_number);
-	}
-
-	free(line);
-	fclose(file);
+	free(buffer);
 	free_stack(stack);
-	return (EXIT_SUCCESS);
+	fclose(file);
+	exit(status);
 }
