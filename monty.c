6d00b3c0ea272abccff27db53687b26f81c5441c
rabialoukhai2 <134720091+rabialoukhai2@users.noreply.github.com>
2023-09-21 13:18:47 +0100

    Update monty.c

--- monty.c
+++ monty.c
@@ -6,10 +6,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 /**
  *main - Entry point for the Monty interpreter
- *
  *@argc: Number of command-line arguments
  *@argv: Array of pointers to command-line argument strings
  *
@@ -17,29 +15,20 @@
  */
 int main(int argc, char **argv)
 {
-	/**
-	 *script_fd - File pointer for the Monty script
-	 */
+
 	FILE *script_fd = NULL;
 
-	/**
-	 *exit_code - Exit code to be returned
-	 */
 	int exit_code = EXIT_SUCCESS;
 
-	/*Check the number of command-line arguments */
 	if (argc != 2)
 		return (msg_err());
 
-	/*Attempt to open the script file */
 	script_fd = fopen(argv[1], "r");
 	if (script_fd == NULL)
 		return (file_open_err(argv[1]));
 
-	/*Run the Monty interpreter */
 	exit_code = monty_runs(script_fd);
 
-	/*Close the script file */
 	fclose(script_fd);
 
 	return (exit_code);
