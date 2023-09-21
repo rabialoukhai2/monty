0ad67964fae8d6e5d4fb2069dc59544d7a262b13
rabialoukhai2 <134720091+rabialoukhai2@users.noreply.github.com>
2023-09-21 13:22:53 +0100

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,16 +1,24 @@
 #include "monty.h"
 
 /**
- *pall - Prints all the values on the stack.
- *@stack: Pointer to the stack.
+ *pall - Prints all the elements in the stack or queue.
+ *@stack: A pointer to the stack or queue.
+ *@value_ln: The line number (unused in this function).
+ *
+ *Description: This function prints all the elements in the stack or queue.
+ *It starts from the top of the stack (or front of the queue) and iterates
+ *through the elements, printing each one.
  */
-void pall(stack_t **stack)
-{
-	stack_t *temp = *stack;
 
-	while (temp)
+void pall(stack_t **stack, unsigned int value_ln)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
 	}
+
+	(void) value_ln;
 }
